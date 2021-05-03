4958322f59048653fce640ba1702692ddb8dddc0
DELL <DELL@RGANAPATHI>
2021-05-03 12:24:55 +0530

    First maven project

---
+++ maven/src/test/java/com/sample/maven/maven_demotest.java
@@ -0,0 +1,15 @@
+package com.sample.maven;
+
+import org.testng.annotations.Test;
+
+public class maven_demotest 
+{
+	
+@Test
+public void login()
+{
+	System.out.println("hello world");
+	
+}
+
+}
